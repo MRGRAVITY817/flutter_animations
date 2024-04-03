7f3d66d5c6ea9c5c5d452d243d1edf86f1b392d3
Hoon Wee <mrgravity817@gmail.com>
2024-04-03 23:14:42 +0900

    feat: add empty implicit animations screen

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations/screens/implicit_animations_screen.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    void goToPage(Widget page) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => page,
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter Animations"),
@@ -13,7 +23,7 @@ class MenuScreen extends StatelessWidget {
         child: Column(
           children: [
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () => goToPage(const ImplicitAnimationsScreen()),
               child: const Text("Implicit Animations"),
             ),
           ],
