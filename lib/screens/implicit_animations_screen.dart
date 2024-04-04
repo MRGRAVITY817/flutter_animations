4e6d6af1380cf89fb4dad7a49d8fb8d9fd047d1b
Hoon Wee <mrgravity817@gmail.com>
2024-04-04 10:12:22 +0900

    show implicit animation examples

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ImplicitAnimationsScreen extends StatefulWidget {
   const ImplicitAnimationsScreen({super.key});
@@ -21,8 +19,6 @@ class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Implicit Animations"),
@@ -31,13 +27,47 @@ class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // AnimatedContainer is a widget that automatically animates changes to its properties.
+            // Nested Implicit Animations Example
+            AnimatedAlign(
+              alignment: _visible ? Alignment.topLeft : Alignment.topRight,
+              duration: const Duration(seconds: 1),
+              curve: Curves.easeInOut,
+              child: AnimatedOpacity(
+                  opacity: _visible ? 1 : 0,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.bounceInOut,
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    color: _visible ? Colors.lime : Colors.blueGrey,
+                  )),
+            ),
+            const SizedBox(height: 32),
+            // Animated Container Example
+            AnimatedContainer(
+              duration: const Duration(seconds: 1),
+              curve: Curves.elasticOut,
+              decoration: BoxDecoration(
+                color: _visible ? Colors.teal : Colors.amber,
+                borderRadius: _visible
+                    ? BorderRadius.circular(0)
+                    : BorderRadius.circular(100),
+              ),
+              width: _visible ? 200 : 100,
+              height: _visible ? 100 : 200,
+              transform:
+                  _visible ? Matrix4.rotationZ(0) : Matrix4.rotationZ(0.5),
+              transformAlignment: Alignment.center,
+            ),
+            const SizedBox(height: 32),
+            // Tween Animation Builder Example
             TweenAnimationBuilder(
                 tween: ColorTween(begin: Colors.red, end: Colors.green),
                 duration: const Duration(seconds: 1),
+                curve: Curves.bounceInOut,
                 builder: (context, value, child) {
                   return Image.network(
-                    "https://picsum.photos/200/300",
+                    "https://picsum.photos/200/180",
                     color: value,
                     colorBlendMode: BlendMode.modulate,
                   );
