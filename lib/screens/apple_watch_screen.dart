cccc9ae209f53f4b740a6833be8cb75e08053194
Hoon Wee <mrgravity817@gmail.com>
2024-04-04 16:13:54 +0900

    draw arcs above circles

--- lib/screens/apple_watch_screen.dart
+++ lib/screens/apple_watch_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AppleWatchScreen extends StatelessWidget {
@@ -15,7 +17,7 @@ class AppleWatchScreen extends StatelessWidget {
       body: Center(
         child: CustomPaint(
           // Creates 400 x 400 canvas
-          size: const Size(400, 400),
+          size: const Size(300, 300),
           painter: AppleWatchPainter(),
         ),
       ),
@@ -24,24 +26,97 @@ class AppleWatchScreen extends StatelessWidget {
 }
 
 class AppleWatchPainter extends CustomPainter {
+  static const strokeWidth = 22.0;
   @override
   void paint(Canvas canvas, Size size) {
-    final rect = Rect.fromLTWH(0, 0, size.width, size.height);
-    final rectPaint = Paint()
-      ..color = Colors.teal.shade200
-      ..style = PaintingStyle.fill;
-
-    canvas.drawRect(rect, rectPaint);
-
-    final circlePaint = Paint()
-      ..color = Colors.pink.shade200
+    // draw red circle
+    final Paint redCirclePaint = Paint()
+      ..color = Colors.red.withOpacity(0.4)
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 10;
+      ..strokeWidth = strokeWidth;
 
     canvas.drawCircle(
       Offset(size.width / 2, size.height / 2),
       size.width / 2,
-      circlePaint,
+      redCirclePaint,
+    );
+
+    // draw red arc
+    final Paint redArcPaint = Paint()
+      ..color = Colors.redAccent.shade400
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth
+      ..strokeCap = StrokeCap.round;
+
+    canvas.drawArc(
+      Rect.fromCircle(
+        center: Offset(size.width / 2, size.height / 2),
+        radius: size.width / 2,
+      ),
+      -0.5 * pi,
+      0.5 * pi,
+      false,
+      redArcPaint,
+    );
+
+    // draw green circle
+    final Paint greenCirclePaint = Paint()
+      ..color = Colors.limeAccent.withOpacity(0.4)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth;
+
+    canvas.drawCircle(
+      Offset(size.width / 2, size.height / 2),
+      size.width / 2.5,
+      greenCirclePaint,
+    );
+
+    // draw green arc
+    final Paint greenArcPaint = Paint()
+      ..color = Colors.limeAccent.shade400
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth
+      ..strokeCap = StrokeCap.round;
+
+    canvas.drawArc(
+      Rect.fromCircle(
+        center: Offset(size.width / 2, size.height / 2),
+        radius: size.width / 2.5,
+      ),
+      -0.5 * pi,
+      1.2 * pi,
+      false,
+      greenArcPaint,
+    );
+
+    // draw blue circle
+    final Paint blueCirclePaint = Paint()
+      ..color = Colors.cyanAccent.withOpacity(0.4)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 25;
+
+    canvas.drawCircle(
+      Offset(size.width / 2, size.height / 2),
+      size.width / 3.44,
+      blueCirclePaint,
+    );
+
+    // draw blue arc
+    final Paint blueArcPaint = Paint()
+      ..color = Colors.cyanAccent.shade400
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 25
+      ..strokeCap = StrokeCap.round;
+
+    canvas.drawArc(
+      Rect.fromCircle(
+        center: Offset(size.width / 2, size.height / 2),
+        radius: size.width / 3.44,
+      ),
+      -0.5 * pi,
+      1.6 * pi,
+      false,
+      blueArcPaint,
     );
   }
 
