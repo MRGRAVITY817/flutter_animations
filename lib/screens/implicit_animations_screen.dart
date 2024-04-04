458148d7c69478862ce1a4deb61595ca385e2bc6
Hoon Wee <mrgravity817@gmail.com>
2024-04-04 09:41:35 +0900

    demostrate implicit animations in flutter

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -9,8 +9,44 @@ class ImplicitAnimationsScreen extends StatefulWidget {
 }
 
 class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
+  bool _visible = true;
+
+  void _trigger() {
+    setState(() {
+      _visible = !_visible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Implicit Animations"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Widgets with "Animated" prefix are implicit animations
+            AnimatedOpacity(
+              duration: const Duration(seconds: 1),
+              opacity: _visible ? 1 : 0,
+              child: Container(
+                width: size.width * 0.8,
+                height: size.width * 0.8,
+                color: Colors.amber,
+              ),
+            ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: _trigger,
+              child: Text(_visible ? "Hide" : "Show"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
