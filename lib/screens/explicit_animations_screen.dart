825fdc511478add6bac7a04f4353072d02d1af89
Hoon Wee <mrgravity817@gmail.com>
2024-04-04 10:53:37 +0900

    demonstrate scale animation in flutter

--- lib/screens/explicit_animations_screen.dart
+++ lib/screens/explicit_animations_screen.dart
@@ -7,12 +7,30 @@ class ExplicitAnimationsScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     // Ticker runs the callback every frame
-    // SingleTickerProvider will run only on this widget
+    // SingleTickerProviderMixin will run ticker only on this widget
     final tickerProvider = useSingleTickerProvider();
+    // So why we need a "ticker"?
+    // Our animation controller will need a "ticker" to run the animation
+    // ** if you have multiple animations, you can use TickerProviderStateMixin
     final controller = useAnimationController(
       duration: const Duration(seconds: 2),
       vsync: tickerProvider,
     );
+
+    void play() {
+      // Run the animation forward
+      controller.forward();
+    }
+
+    void pause() {
+      // Stop the animation
+      controller.stop();
+    }
+
+    void rewind() {
+      // Run the animation in reverse
+      controller.reverse();
+    }
 
     return Scaffold(
       appBar: AppBar(title: const Text('Explicit Animations')),
@@ -21,16 +39,27 @@ class ExplicitAnimationsScreen extends HookWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ScaleTransition(
-                scale: controller, child: const FlutterLogo(size: 200)),
-            ElevatedButton(
-              onPressed: () {
-                if (controller.isCompleted) {
-                  controller.reverse();
-                } else {
-                  controller.forward();
-                }
-              },
-              child: Text(controller.isCompleted ? 'Reverse' : 'Forward'),
+              scale: controller,
+              child: const FlutterLogo(size: 200),
+            ),
+            const SizedBox(height: 20),
+            Wrap(
+              direction: Axis.horizontal,
+              spacing: 10,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: play,
+                  child: const Text('Play'),
+                ),
+                ElevatedButton(
+                  onPressed: pause,
+                  child: const Text('Pause'),
+                ),
+                ElevatedButton(
+                  onPressed: rewind,
+                  child: const Text('Rewind'),
+                ),
+              ],
             ),
           ],
         ),
