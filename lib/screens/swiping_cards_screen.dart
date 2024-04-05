fdff3dddd0aede5fafca65a5281c1dd43255b422
Hoon Wee <mrgravity817@gmail.com>
2024-04-05 09:38:03 +0900

    add animations to swiping card screen

--- lib/screens/swiping_cards_screen.dart
+++ lib/screens/swiping_cards_screen.dart
@@ -7,42 +7,59 @@ class SwipingCardsScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    final posX = useState(0.0);
+
+    final tickerProvider = useSingleTickerProvider();
+    final controller = useAnimationController(
+      vsync: tickerProvider,
+      duration: const Duration(milliseconds: 500),
+      // Since animation controller limits the bound between 0 and 1 by default,
+      // we need to set the lower and upper bounds to the screen width
+      lowerBound: -size.width,
+      upperBound: size.width,
+    );
 
     void onDragUpdate(DragUpdateDetails details) {
-      posX.value += details.delta.dx;
+      controller.value += details.delta.dx;
     }
 
     void onDragEnd(DragEndDetails details) {
-      posX.value = 0;
+      controller.animateTo(
+        0,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.bounceOut,
+      );
     }
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Swiping Cards"),
       ),
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: GestureDetector(
-              onHorizontalDragUpdate: onDragUpdate,
-              onHorizontalDragEnd: onDragEnd,
-              child: Transform.translate(
-                offset: Offset(posX.value, 0),
-                child: Material(
-                  elevation: 10,
-                  child: SizedBox(
-                    width: size.width * 0.8,
-                    height: size.height * 0.8,
-                    child: Container(color: Colors.red),
+      body: AnimatedBuilder(
+          animation: controller,
+          builder: (context, child) {
+            return Stack(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: GestureDetector(
+                    onHorizontalDragUpdate: onDragUpdate,
+                    onHorizontalDragEnd: onDragEnd,
+                    child: Transform.translate(
+                      offset: Offset(controller.value, 0),
+                      child: Material(
+                        elevation: 10,
+                        child: SizedBox(
+                          width: size.width * 0.8,
+                          height: size.height * 0.8,
+                          child: Container(color: Colors.red),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ),
-        ],
-      ),
+              ],
+            );
+          }),
     );
   }
 }
