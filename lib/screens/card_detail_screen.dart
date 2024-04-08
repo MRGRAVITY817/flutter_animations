fb645557816100914c200a24e767dcfdced62d48
Hoon Wee <mrgravity817@gmail.com>
2024-04-08 23:07:01 +0900

    feat: add transaction items, animated

--- lib/screens/card_detail_screen.dart
+++ lib/screens/card_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_animations/screens/credit_card.dart';
 
 class CardDetailScreen extends StatelessWidget {
@@ -25,6 +26,45 @@ class CardDetailScreen extends StatelessWidget {
                 isExpanded: false,
               ),
             ),
+            ...[
+              for (final _ in List.generate(6, (_) => 0))
+                Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  child: ListTile(
+                    tileColor: Colors.grey.shade100,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    leading: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.red,
+                      ),
+                      child: const Icon(
+                        Icons.shopping_bag,
+                        color: Colors.white,
+                      ),
+                    ),
+                    title: const Text(
+                      "Uniqlo",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: const Text("T-Shirts"),
+                    trailing: const Text(
+                      "-\$10",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+            ]
+                .animate(interval: 500.ms)
+                .fadeIn(begin: 0)
+                .flipV(begin: -0.5, end: 0, curve: Curves.bounceOut)
           ],
         ),
       ),
