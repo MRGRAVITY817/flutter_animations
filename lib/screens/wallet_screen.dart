ec8448c5d89d601f78b71de535b2ab963a4332b4
Hoon Wee <mrgravity817@gmail.com>
2024-04-08 22:17:52 +0900

    feat: animate list of cards

--- lib/screens/wallet_screen.dart
+++ lib/screens/wallet_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_animations/screens/credit_card.dart';
 
 class WalletScreen extends StatelessWidget {
@@ -10,15 +11,31 @@ class WalletScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Wallet"),
       ),
-      body: const Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              CreditCard(bgColor: Colors.purple),
-              CreditCard(bgColor: Colors.black),
-              CreditCard(bgColor: Colors.blue),
-            ],
-          )),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          // children: AnimateList(
+          //   interval: 500.ms,
+          //   effects: [
+          //     const SlideEffect(begin: Offset(-1, 0), end: Offset.zero),
+          //     const FadeEffect(begin: 0.0, end: 1.0)
+          //   ],
+          //   children: [
+          //     const CreditCard(bgColor: Colors.purple),
+          //     const CreditCard(bgColor: Colors.black),
+          //     const CreditCard(bgColor: Colors.blue),
+          //   ],
+          // ),
+          children: [
+            const CreditCard(bgColor: Colors.purple),
+            const CreditCard(bgColor: Colors.black),
+            const CreditCard(bgColor: Colors.blue),
+          ]
+              .animate(interval: 500.ms)
+              .fadeIn(begin: 0)
+              .slideX(begin: -1, end: 0),
+        ),
+      ),
     );
   }
 }
