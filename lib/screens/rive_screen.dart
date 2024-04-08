38ab2b5fcd178c1f51a64cce5ac1088be7c61658
Hoon Wee <mrgravity817@gmail.com>
2024-04-08 12:51:55 +0900

    add old man animation and panel toggle button

--- lib/screens/rive_screen.dart
+++ lib/screens/rive_screen.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
-class RiveScreen extends StatelessWidget {
+class RiveScreen extends StatefulWidget {
   const RiveScreen({super.key});
+
+  @override
+  State<RiveScreen> createState() => _RiveScreenState();
+}
+
+class _RiveScreenState extends State<RiveScreen> {
+  late final StateMachineController _stateMachineController;
+
+  void _onInit(Artboard artboard) {
+    _stateMachineController = StateMachineController.fromArtboard(
+      artboard,
+      "state",
+    )!;
+    artboard.addController(_stateMachineController);
+  }
+
+  void _togglePanel() {
+    final input = _stateMachineController.findInput<bool>("panelActive")!;
+    input.change(!input.value);
+  }
+
+  @override
+  void dispose() {
+    _stateMachineController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +39,20 @@ class RiveScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            SizedBox(height: 400),
+            SizedBox(
+              height: 400,
+              width: double.infinity,
+              child: RiveAnimation.asset(
+                "assets/animations/old-man-animation.riv",
+                artboard: "main",
+                stateMachines: const ["state"],
+                onInit: _onInit,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _togglePanel,
+              child: const Text("Toggle panel"),
+            ),
           ],
         ),
       ),
