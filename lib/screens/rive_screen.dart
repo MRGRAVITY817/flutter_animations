fddc6920dec50ec3dfa54dd635fc31bda089837e
Hoon Wee <mrgravity817@gmail.com>
2024-04-08 13:26:30 +0900

    feat: add balls animation

--- lib/screens/rive_screen.dart
+++ lib/screens/rive_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
@@ -9,42 +11,29 @@ class RiveScreen extends StatefulWidget {
 }
 
 class _RiveScreenState extends State<RiveScreen> {
-  late final StateMachineController _stateMachineController;
-
-  void _onInit(Artboard artboard) {
-    _stateMachineController = StateMachineController.fromArtboard(
-      artboard,
-      "state",
-      onStateChange: (stateMachineName, stateName) {
-        print("$stateMachineName - $stateName");
-      },
-    )!;
-    artboard.addController(_stateMachineController);
-  }
-
-  @override
-  void dispose() {
-    _stateMachineController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Rive Animations"),
-      ),
-      body: Center(
-        child: Container(
-          color: Colors.pinkAccent,
-          width: double.infinity,
-          child: RiveAnimation.asset(
-            "assets/animations/stars-animation.riv",
-            artboard: "artboard",
-            stateMachines: const ["state"],
-            onInit: _onInit,
+      body: Stack(
+        children: [
+          const RiveAnimation.asset(
+            "assets/animations/balls_animation.riv",
+            fit: BoxFit.cover,
           ),
-        ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+              child: const Center(
+                child: Text(
+                  "Welcome to AI App",
+                  style: TextStyle(
+                    fontSize: 28,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
